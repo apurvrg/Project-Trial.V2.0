e9f2034f7db441834389b6fc65fee52754b1d388
Apurv Gawande <59498870+apurvrg@users.noreply.github.com>
2020-09-24 19:09:40 +0530

    Update SecurityConfig.java

--- src/main/java/ProjectTrial/config/SecurityConfig.java
+++ src/main/java/ProjectTrial/config/SecurityConfig.java
@@ -33,8 +33,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
-			.antMatchers("/management/**").hasAnyRole("STUDENT","MANAGER", "ADMIN")
-			.antMatchers("/java/**").hasAnyRole("STUDENT", "MANAGER", "ADMIN")
+			.antMatchers("/management/**").hasAnyRole("STUDENT","MANAGER")
+			.antMatchers("/java/**").hasAnyRole("STUDENT", "MANAGER")
 			.antMatchers("/resources/**").permitAll()
 			.and()
 			.formLogin()
